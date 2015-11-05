e1dcff5fa34a1bc06222bd23c043868bbfbc03ac
jasoncreynolds <jreyno14@uncc.edu>
2015-11-05 02:40:31 +0000

    added movie.rb code

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+    
+  def self.find_all_by_rating(ratings)
+      where(arel_table[:rating].in(ratings))
+  end 
+  
+  def self.ratings 
+    ['G','PG','PG-13','R']
+  end
+  
 end
